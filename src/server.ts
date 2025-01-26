910cf375b08d32f83f5f3eada149509da0f1c14f
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-01-25 21:19:16 -0300

    using raw to registries insert

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,13 @@ app.use(express.json());
 app.post("/courses", async (request: Request, response: Response) => {
     const { name } = request.body;
 
+    //Fazendo inserção de registros com knex e query builder
     await knex("courses").insert({ name });
+
+    /* Outra maneira de fazer inserção através do método raw onde você consegue utilizar comandos sql ao invés das funções do knex e query builder
+    
+    await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
+    */
 
     response.status(201).json();
 });
